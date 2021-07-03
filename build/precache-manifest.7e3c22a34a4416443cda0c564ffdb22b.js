b22b654f425e78acf32e0c2307a29c6d50d0f929
Christy-clair <christyclair15@gmail.com>
2021-07-04 00:10:29 +0530

    updated

---
+++ build/precache-manifest.7e3c22a34a4416443cda0c564ffdb22b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9ffffeb7844254f66ff",
+    "url": "/static/css/main.06565249.chunk.css"
+  },
+  {
+    "revision": "d9ffffeb7844254f66ff",
+    "url": "/static/js/main.d9ffffeb.chunk.js"
+  },
+  {
+    "revision": "81fc0683cfdf6fb9afc3",
+    "url": "/static/js/1.81fc0683.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "de60dc93ad338cd90faabcf591af96ee",
+    "url": "/index.html"
+  }
+];
